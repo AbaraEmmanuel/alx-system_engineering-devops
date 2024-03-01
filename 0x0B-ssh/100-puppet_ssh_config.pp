610ac64776da57e11e6552a92876ed33b234fb88
AbaraEmmanuel <emmanuelabara265@gmail.com>
2024-03-01 15:49:35 -0800

    Making additional file executable

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#using pupet to connect witout passsword
+
+file { '/etc/ssh/ssh_config':
+  ensure => present,
+}
+
+file_line { 'Turn off passwd auth':
+  path    => '/etc/ssh/ssh_config',
+  line    => 'PasswordAuthentication no',
+  match   => '^#PasswordAuthentication',
+}
+
+file_line { 'Declare identity file':
+  path    => '/etc/ssh/ssh_config',
+  line    => 'IdentityFile ~/.ssh/school',
+  match   => '^IdentityFile',
+}
